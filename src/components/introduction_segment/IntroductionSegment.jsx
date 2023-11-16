88bf32c3af708575927f5fa7bb796b79bc80315c
KrisHub94 <kristianhuber@hotmail.com>
2023-11-16 15:38:32 +0100

    created introduction segment component

---
+++ src/components/introduction_segment/IntroductionSegment.jsx
@@ -0,0 +1,8 @@
+const IntroductionSegment = () => {
+    return (
+        <div id="introduction-segment">
+            <p>This is where the Introduction Segment should be.</p>
+        </div>
+    )
+}
+export default IntroductionSegment;
