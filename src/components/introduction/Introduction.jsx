65e3771c19393ac0f55207f580e3d51a96c16a82
KrisHub94 <kristianhuber@hotmail.com>
2023-11-16 17:25:05 +0100

    added text to the introduction to increase height of page for navbar testing

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -1,7 +1,14 @@
 const Introduction = () => {
     return (
         <section id="introduction">
-            <p>This is where the Introduction Segment should be.</p>
+            <h1>KRISTIAN HUBER</h1>
+            <h2>The man with the magic fingers</h2>
+            <p>
+                Hello! My name is Kristian Huber, but my friends call me Kris. I am possibly the best Full Stack Software Developer
+                that the world has ever seen. I recently finished my training at Codecool Austria and am now proficient in a number of technologies
+                and programming languages, such as HTML, CSS, JavaScript, SQL, React.js, Spring Boot, MongoDB and many more.
+            </p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex cupiditate dicta explicabo ducimus eveniet mollitia voluptas ullam illum fuga sit recusandae ad quo, omnis quasi, sapiente, reprehenderit consequatur. Esse, nobis! Harum rem asperiores architecto molestiae ipsum delectus consequatur repellendus nulla quaerat facere error, voluptatibus ullam accusamus facilis ipsam natus non minima nesciunt ea obcaecati voluptates, nihil sint ex! Perspiciatis rerum dignissimos deserunt magni minus laboriosam corrupti, autem ea. Eligendi rerum provident officia adipisci libero error dolore porro exercitationem quam autem quo natus vero veritatis optio harum ullam, doloribus, enim modi culpa aut voluptatem! Porro possimus quo placeat rerum eveniet inventore pariatur corrupti officiis maxime sit temporibus molestiae eos enim voluptatibus expedita qui voluptatum vero, vitae perferendis repellendus iure veritatis odit dolorum reiciendis? Eos quam quo, voluptates accusamus soluta nobis in quod non excepturi. Dignissimos ea expedita temporibus nihil consectetur dolore delectus perspiciatis natus, eum voluptas possimus porro quis dolorem corrupti vel alias magnam vitae libero quam. Explicabo sapiente, esse, iusto dignissimos ut natus ipsam minus porro beatae qui, rerum atque expedita sequi ea soluta accusamus placeat nihil corporis? Ullam beatae nesciunt cum nostrum id, aliquid unde earum magni! Recusandae, eum. Magni voluptates vitae odio consequuntur quasi? Recusandae exercitationem at temporibus?</p>
         </section>
     )
 }
