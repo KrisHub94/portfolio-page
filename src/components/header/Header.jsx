5d506795ca6b06917125957e67bcc97319cb5bad
KrisHub94 <kristianhuber@hotmail.com>
2023-11-16 17:11:23 +0100

    added css file for header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,19 @@
+import "./Header.css";
+
 const Header = () => {
     return (
         <header id="header">
             <div className="main-wrapper">
-                <div className="sub-wrapper">
-                    <h3>Kristian Huber</h3>
-                    <h3>FULL STACK SOFTWARE DEVELOPER</h3>
-                </div>
-                <div className="sub-wrapper">
-                    <a>Projects</a>
-                    <a>Contact</a>
-                </div>
+                <nav className="nav-main">
+                    <div className="sub-wrapper">
+                        <p>Kristian Huber</p>
+                        <p>FULL STACK SOFTWARE DEVELOPER</p>
+                    </div>
+                    <div className="sub-wrapper">
+                        <a>Projects</a>
+                        <a>Contact</a>
+                    </div>
+                </nav>
             </div>
         </header>
     )
