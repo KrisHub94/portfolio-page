f201e9761777e40390c3061b270580fdd81093e6
KrisHub94 <kristianhuber@hotmail.com>
2023-11-16 18:21:53 +0100

    implemented new navbarlink component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,13 +7,12 @@ const Header = () => {
             <div className="main-wrapper">
                 <nav className="nav-main">
                     <div className="sub-wrapper">
-                        <a href="#introduction">Kristian Huber</a>
+                        <NavbarLink text={"Kristian Huber"} target={"introduction"} />
                         <span>FULL STACK SOFTWARE DEVELOPER</span>
                     </div>
                     <div className="sub-wrapper">
-                        <a href="#project-display">Projects</a>
-                        <a href="#contact-info">Contact</a>
-                        <NavbarLink name={"Projects"} target={"project-display"} />
+                        <NavbarLink text={"Projects"} target={"project-display"} />
+                        <NavbarLink text={"Contact"} target={"contact-info"} />
                     </div>
                 </nav>
             </div>
