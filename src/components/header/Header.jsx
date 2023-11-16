8ce8625d97eea0c6ea9c01afefbe05f516143b67
KrisHub94 <kristianhuber@hotmail.com>
2023-11-16 16:57:50 +0100

    added placeholder elements to header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,17 @@
 const Header = () => {
     return (
-        <div id="header">
-            <p>This is where the Header should be.</p>
-        </div>
+        <header id="header">
+            <div className="main-wrapper">
+                <div className="sub-wrapper">
+                    <h3>Kristian Huber</h3>
+                    <h3>FULL STACK SOFTWARE DEVELOPER</h3>
+                </div>
+                <div className="sub-wrapper">
+                    <a>Projects</a>
+                    <a>Contact</a>
+                </div>
+            </div>
+        </header>
     )
 }
 export default Header;
