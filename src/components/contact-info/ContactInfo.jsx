bde4bd939f2961f79e06d1954d9dfebc6e0332be
KrisHub94 <kristianhuber@hotmail.com>
2023-11-16 15:42:34 +0100

    created contact info component

---
+++ src/components/contact-info/ContactInfo.jsx
@@ -0,0 +1,8 @@
+const ContactInfo = () => {
+    return (
+        <div id="contact-info">
+            <p>This is where the Contact Info should be.</p>
+        </div>
+    )
+}
+export default ContactInfo;
