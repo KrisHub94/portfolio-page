a35132e564ae3ca9c9c5a9016b3ef61c14510299
KrisHub94 <kristianhuber@hotmail.com>
2023-11-17 15:33:06 +0100

    started adding css to create layout, added borders to see elements properly

--- src/components/project_display/ProjectDisplay.jsx
+++ src/components/project_display/ProjectDisplay.jsx
@@ -1,9 +1,11 @@
+import "./ProjectDisplay.css";
+
 const ProjectDisplay = () => {
     return (
         <section id="project-display">
             <div className="section-container">
                 <div className="arrow-left"></div>
-                <div className="display-container"></div>
+                <div id="display-container"></div>
                 <div className="arrow-right"></div>
             </div>
         </section>
