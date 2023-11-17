2cd3af77c94551ac17f789eff868983fa4fdfa48
KrisHub94 <kristianhuber@hotmail.com>
2023-11-17 17:31:52 +0100

    added displayed project

--- src/components/project_display/ProjectData.js
+++ src/components/project_display/ProjectData.js
@@ -1,15 +1,18 @@
 const PROJECTS = [
     {
+        id: 0,
         name: "Project 1",
         imgSrc: "https://placehold.co/600x400",
         description: "This is Project 1. It is named like that because it is the first project that i want to display.",
     },
     {
+        id: 1,
         name: "Project 2",
         imgSrc: "https://placehold.co/600x400",
         description: "This is the second project. It is really cool. Codecool.",
     },
     {
+        id: 2,
         name: "Project 3",
         imgSrc: "https://placehold.co/600x400",
         description: "Project 3 was very interesting because, like the other placeholder projects, I didn't actually make it.",
--- src/components/project_display/ProjectDisplay.jsx
+++ src/components/project_display/ProjectDisplay.jsx
@@ -1,13 +1,36 @@
 import "./ProjectDisplay.css";
+import PROJECTS from "./ProjectData";
+import { useState } from "react";
 
 const ProjectDisplay = () => {
+    const [displayedIndex, setDisplayedIndex] = useState(0);
     return (
         <section id="project-display">
             <h2>HERE ARE SOME OF MY PROJECTS</h2>
             <div className="section-container">
                 <div className="arrow-left"></div>
                 <div id="display-container">
-                    <div id="display-names-container"></div>
+                    <div id="display-names-container" className="sub-wrapper">
+                        {
+                            PROJECTS.map(project => {
+                                return <button 
+                                key={project.id} 
+                                onClick={() => {
+                                    setDisplayedIndex(project.id);
+                                    }}
+                                >
+                                {project.name}
+                                </button>
+                            })
+                        }
+                    </div>
+                    <div id="displayed-project">
+                        <img src={PROJECTS[displayedIndex].imgSrc} alt="a picture of my project"></img>
+                        <div>
+                            <h3>{PROJECTS[displayedIndex].name}</h3>
+                            <p>{PROJECTS[displayedIndex].description}</p>
+                        </div>
+                    </div>
                 </div>
                 <div className="arrow-right"></div>
             </div>
