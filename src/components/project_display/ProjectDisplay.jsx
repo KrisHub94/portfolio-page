a81bb1e1aab58645c4bad2c31e63697262c8f5cc
KrisHub94 <kristianhuber@hotmail.com>
2023-11-17 16:39:10 +0100

    added header to section

--- src/components/project_display/ProjectDisplay.jsx
+++ src/components/project_display/ProjectDisplay.jsx
@@ -3,6 +3,7 @@ import "./ProjectDisplay.css";
 const ProjectDisplay = () => {
     return (
         <section id="project-display">
+            <h2>HERE ARE SOME OF MY PROJECTS</h2>
             <div className="section-container">
                 <div className="arrow-left"></div>
                 <div id="display-container"></div>
