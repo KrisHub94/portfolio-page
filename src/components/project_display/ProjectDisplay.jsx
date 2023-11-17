caa2c2b77eef2839a8236c1c23c09d2870f86c74
KrisHub94 <kristianhuber@hotmail.com>
2023-11-17 16:47:21 +0100

    added project data file for the displayed projects

--- src/components/project_display/ProjectDisplay.jsx
+++ src/components/project_display/ProjectDisplay.jsx
@@ -6,7 +6,9 @@ const ProjectDisplay = () => {
             <h2>HERE ARE SOME OF MY PROJECTS</h2>
             <div className="section-container">
                 <div className="arrow-left"></div>
-                <div id="display-container"></div>
+                <div id="display-container">
+                    <div id="display-names-container"></div>
+                </div>
                 <div className="arrow-right"></div>
             </div>
         </section>
