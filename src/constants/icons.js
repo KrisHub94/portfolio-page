b560ff2b0c1ca09086155afca7512ffdf9109eba
KrisHub94 <kristianhuber@hotmail.com>
2023-11-20 16:55:56 +0100

    added more icon data to constants for later use

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -11,5 +11,45 @@ const ICONS = {
         LINK: "https://www.linkedin.com/in/kristian-huber-305a2429a/",
         ICON_CLASSNAME: "ci-linkedin",
     },
+    CSS: {
+        LINK: "",
+        ICON_CLASSNAME: "ci-css",
+    },
+    DISCORD: {
+        LINK: "discordapp.com/users/1022838020380033044",
+        ICON_CLASSNAME: "ci-discord",
+    },
+    HTML: {
+        LINK: "",
+        ICON_CLASSNAME: "ci-html",
+    },
+    JAVA: {
+        LINK: "",
+        ICON_CLASSNAME: "ci-java",
+    },
+    JAVASCRIPT: {
+        LINK: "",
+        ICON_CLASSNAME: "ci-js",
+    },
+    MONGODB: {
+        LINK: "",
+        ICON_CLASSNAME: "ci-mongodb",
+    },
+    NODEJS: {
+        LINK: "",
+        ICON_CLASSNAME: "ci-nodejs",
+    },
+    POSTGRESQL: {
+        LINK: "",
+        ICON_CLASSNAME: "ci-postgresql",
+    },
+    REACTJS: {
+        LINK: "",
+        ICON_CLASSNAME: "ci-reactjs",
+    },
+    SPRING: {
+        LINK: "",
+        ICON_CLASSNAME: "ci-spring",
+    },
 }
 export default ICONS;
