18033732bbbeaffd4042a0ffe796c208f62eaeb9
KrisHub94 <kristianhuber@hotmail.com>
2023-11-28 17:57:46 +0100

    added more circles

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -6,6 +6,12 @@ const Background = () => {
             <svg className="circle1" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="50%" cy="50%" r="80" fill="none" stroke="black" strokeWidth="20" strokeDasharray="2"></circle>
             </svg>
+            <svg className="circle2" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="50%" cy="50%" r="110" fill="none" stroke="black" strokeWidth="20" strokeDasharray="2"></circle>
+            </svg>
+            <svg className="circle3" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="50%" cy="50%" r="140" fill="none" stroke="black" strokeWidth="20" strokeDasharray="2"></circle>
+            </svg>
         </div>
     )
 }
