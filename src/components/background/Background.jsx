e5f4f4dce6a4745a379c79af7ca0ab3a1c8b45fe
KrisHub94 <kristianhuber@hotmail.com>
2023-12-12 16:47:44 +0100

    made background circle radius responsive

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -2,11 +2,12 @@ import "./Background.css";
 import BackgroundCircle from "./BackgroundCircle";
 
 const Background = () => {
+    const isOnMobile = window.innerWidth > 600?false:true;
     return(
         <div id="background-main">
-            <BackgroundCircle number={1} radius={140} />
-            <BackgroundCircle number={2} radius={190} />
-            <BackgroundCircle number={3} radius={240} />
+            <BackgroundCircle number={1} radius={isOnMobile?80:140} />
+            <BackgroundCircle number={2} radius={isOnMobile?130:190} />
+            <BackgroundCircle number={3} radius={isOnMobile?180:240} />
         </div>
     )
 }
