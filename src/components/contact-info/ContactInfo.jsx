7cd08ef7c854b5c7569a8f9108efa23612b321c4
KrisHub94 <kristianhuber@hotmail.com>
2023-12-13 14:41:38 +0100

    minor css tweaks in contact info section

--- src/components/contact-info/ContactInfo.jsx
+++ src/components/contact-info/ContactInfo.jsx
@@ -1,15 +1,15 @@
 import ICONS from "../../constants/icons";
 import ClickableIcon from "../clickable_icon/ClickableIcon";
+import "./ContactInfo.css";
 
 const ContactInfo = () => {
     return (
         <section id="contact-info">
-            <h2>Contact Info - I would love to get in touch with you!</h2>
-            <p>If you want to talk to me about a job or working on a project together, or if you
-                want to give feedback regarding this page, even if you just want to talk about
-                video games please contact me!
+            <h2>Contact Info</h2>
+            <p id="contact-text">If you want to talk to me about a job or working on a project together, or if you
+                want to give feedback regarding this page, even if you just want to say hello
+                 please contact me!
             </p>
-            <i className="cib-codewars"></i>
             <div className="icons-container">
                 <ClickableIcon link={ICONS.GITHUB_LIGHT.LINK} iconClassName={ICONS.GITHUB_LIGHT.ICON_CLASSNAME} />
                 <ClickableIcon link={ICONS.GMAIL.LINK} iconClassName={ICONS.GMAIL.ICON_CLASSNAME} />
