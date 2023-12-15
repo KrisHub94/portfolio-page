6fc6d0ef1570ad730ada7a3c3fdc3763974e0a2a
KrisHub94 <kristianhuber@hotmail.com>
2023-12-15 16:42:27 +0100

    fixed button react standard light mode layout

--- src/components/contact-info/ContactInfo.jsx
+++ src/components/contact-info/ContactInfo.jsx
@@ -7,7 +7,7 @@ const ContactInfo = () => {
         <section id="contact-info">
             <h2>Contact Info</h2>
             <p id="contact-text">If you want to talk to me about a job or working on a project together, or if you
-                want to give feedback regarding this page, even if you just want to say hello
+                want to give feedback regarding this page, even if you just want to say hello,
                  please contact me!
             </p>
             <div className="icons-container">
